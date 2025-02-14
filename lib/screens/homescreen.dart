03dfaacfeb5103bbd4b0e93ffedd6ba58c4b7dab
marlouieknowsz <keithsoliguen@gmail.com>
2025-02-15 02:02:10 +0800

    homescreen.dart update

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -7,7 +7,7 @@ class Homescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Keith'),
+        title: const Text('Keith'),
       ),
     );
   }
