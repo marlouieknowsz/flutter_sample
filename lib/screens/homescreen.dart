263dac362bb5ac7bbb4010d5d80eeb889dbccf02
marlouieknowsz <keithsoliguen@gmail.com>
2025-02-15 02:22:08 +0800

    update homscreen.dart

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -7,7 +7,29 @@ class Homescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Keith'),
+        title: const Text(
+          'Keith',
+          style: TextStyle(fontStyle: FontStyle.italic),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.cyan,
+      ),
+
+      //ITEMS INSIDE OF THE PAGE
+      body: Column(
+        children: [
+          Center(
+            child: Text('Hello World'),
+          ),
+          Container(
+            width: 100,
+            height: 40,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+            ),
+          )
+        ],
       ),
     );
   }
